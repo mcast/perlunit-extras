7facc119b077564e9f5e26c50fe4f6cc94e6ff08
Matthew Astley <mca+github@sanger.ac.uk>
2004-08-02 16:42:15 +0000

    base Test::Unit::TestCase, with added toys
    git-svn-id: svn+ssh://intcvs1/repos/svn/badger/misc/PSG/trunk@12587 5f9e3794-462c-0410-9ee8-a4ce9f1c5c24

---
+++ testlib/McaTestCase.pm
@@ -0,0 +1,34 @@
+#! /usr/local/bin/perl -wc
+
+package McaTestCase;
+
+use base 'Test::Unit::TestCase';
+use Data::Dumper;
+
+# inherit perfectly adequate new, set_up
+
+sub tear_down {
+    my $self = shift;
+
+    # Display data structures from testcase, if we fail
+    if ($self->{annotate}) {
+	my $d = Data::Dumper->new([values %{ $self->{annotate} }], [keys %{ $self->{annotate} }]);
+	$d->Indent(1)->Purity(1)->Quotekeys(0);
+	$self->annotate($d->Dump);
+    }
+
+    $self->SUPER::tear_down;
+}
+
+sub assert_dies {
+    my ($self, $regex, $coderef) = @_;
+    eval {
+	$coderef->();
+    };
+    my $err = $@;
+    $self->fail("Code did not die") unless $err;
+    $self->assert_matches( $regex, $err );
+}
+
+
+1;

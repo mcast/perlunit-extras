dab0754f9de113d8fa1fc881132bb0ced2f383de
Matthew Astley <mca+github@sanger.ac.uk>
2004-08-04 14:43:22 +0000

    assert_dies: accept multi coderefs, all must die; selftest for the test (perverse but did catch a bug)
    git-svn-id: svn+ssh://intcvs1/repos/svn/badger/misc/PSG/trunk@12618 5f9e3794-462c-0410-9ee8-a4ce9f1c5c24

--- testlib/McaTestCase.pm
+++ testlib/McaTestCase.pm
@@ -21,15 +21,71 @@ sub tear_down {
     $self->SUPER::tear_down;
 }
 
+# Assert that each coderef dies with a message matching the regex
 sub assert_dies {
-    my ($self, $regex, $coderef) = @_;
-    eval {
-	$coderef->();
-    };
-    my $err = $@;
+    my ($self, $regex, @coderef) = @_;
+
+    # Need to include caller in failure message, since the builtin
+    # line number reporting is no help
     my @caller = caller();
-    $self->fail("Code did not die, caller at $caller[1] line $caller[2]") unless $err;
-    $self->assert_matches( $regex, $err );
+    my $caller = "caller at $caller[1] line $caller[2]";
+
+    $self->fail("arg1 not Regexp from $caller") unless ref($regex) eq 'Regexp';
+    $self->fail("Bad args (no coderefs) from $caller") unless @coderef;
+    $self->fail("Bad args (coderefs aren't) from $caller") if grep {ref($_) ne 'CODE'} @coderef;
+
+    for (my $i=0; $i<@coderef; $i++) {
+	eval {
+	    $coderef[$i]->();
+	};
+	my $err = $@;
+	$self->fail("Code#$i did not die, $caller") unless $err;
+	$self->assert_matches($regex, "Code#$i '$err'");
+    }
+}
+
+# Self-tests for this base class, might as well piggyback unless/until
+# that wastes too much time
+
+sub test_baseselftest_assert_dies {
+    my $self = shift;
+
+    # Run our assertion so it should pass, check it ran
+    my $flag = 0;
+    $self->assert_dies(qr/dodo style/, sub { $flag = "captured"; die "dodo style"; });
+    $self->assert_str_equals("captured", $flag);
+
+    ### Use the builtin Test::Unit::Assert to check the assertion fails:
+
+    # doesn't die
+    $self->assert_raises('Test::Unit::Failure',
+			 sub {
+			     $self->assert_dies( qr/wibble/, sub { return 1; });
+			 });
+
+    # wrong arg
+    $self->assert_raises('Test::Unit::Failure',
+			 sub {
+			     $self->assert_dies( "foo", sub { return 1; });
+			 });
+
+    # multi coderef, second has wrong message
+    $self->assert_raises('Test::Unit::Failure',
+			 sub {
+			     $self->assert_dies( qr/wibble/,
+						 sub { die "wibble" },
+						 sub { die "spong"  });
+			 });
+
+    # multi coderef, third doesn't die
+    $self->assert_raises('Test::Unit::Failure',
+			 sub {
+			     $self->assert_dies( qr/wibble/,
+						 sub { die "eek wibble bonk" },
+						 sub { die "boo wibble spong" },
+						 sub { return 1; });
+			 });
+
 }
 
 

e5f8d4ce2f5b3292049c04a91d37de92432700ae
Matthew Astley <mca+github@sanger.ac.uk>
2004-09-09 17:03:13 +0000

    tidy up code and API
    git-svn-id: svn+ssh://intcvs1/repos/svn/badger/misc/PSG/trunk@12979 5f9e3794-462c-0410-9ee8-a4ce9f1c5c24

--- testlib/McaTkTestCase.pm
+++ testlib/McaTkTestCase.pm
@@ -3,41 +3,61 @@ package McaTkTestCase;
 
 use base 'McaTestCase'; # Test::Unit::TestCase with extra toys
 
+=head1 NAME
+
+McaTkTestCase - Test::Unit::TestCase with support for Tk::MainWindow-driven test
+
+=cut
+
+# Private accessor for our data stash
+my $get_priv = sub {
+    return $_[0]->{__PACKAGE__."tkstuff"} ||= {};
+};
+
+ ### not implemented .. sleeping with update not needed yet
+ #
+ # # Accessor (one arg get, two arg set) for time delays config
+ # # Defaults to zero, convention suggests milliseconds
+ # sub slowdown {
+ #     my ($self, $what, $delay) = @_;
+ #     my $S = $get_priv->($self)->{slowdown};
+ #     $S->{$what} = $delay if defined $delay;
+ #     return $S->{$what} || 0;
+ # }
+
 sub tear_down {
     my $self = shift;
-    my $P = $self->{__PACKAGE__."tkstuff"} ||= {};
+    my $P = $get_priv->($self);
 
     # Ensure again that windows are gone
     my @zapped;
-    foreach my $win (@{ $P->{windows} }) {
-	next unless Tk::Exists($win);
+    foreach my $win ($self->tk_windows) {
 	push @zapped, $win;
 	$win->destroy;
     }
     $self->assert_str_equals("()", "(@zapped)");
 
     # Last bail-out check
-    $self->fail("Late bail-out check: $P->{bail}") if defined $P->{bail};
+    $self->bail_check("**LATE** Bail-out check during tear_down");
 
     $self->SUPER::tear_down;
 }
 
 
-# Cause test to fail later - for use in Tk callback.
+# Cause test to fail later - for use in Tk callback, where you can't usefully call $self->fail
 # Organise the early destruction of the window
 #
 # Caller's responsibility to abort the current callback in a Tk-friendly way.
 # (Tk->break seems to propagate outside the callback..?)
 sub bail {
     my ($self, $prob) = @_;
-    my $P = $self->{__PACKAGE__."tkstuff"} ||= {};
+    my $P = $get_priv->($self);
 
     if (!$P->{bail}) {
 	# First bail
 	$P->{bail} = $prob;
 	# Direct destruction here upsets the callback mechanism..?
-	foreach my $win (@{ $P->{windows} }) {
-	    next unless Tk::Exists($win);
+	foreach my $win ($self->tk_windows) {
 	    $win->afterIdle(sub { $win->destroy if Tk::Exists($win) })
 	}
     } else {
@@ -48,23 +68,25 @@ sub bail {
     $self->{annotate}->{bail} = $P->{bail} if $self->{annotate};
 }
 
-
+# Fail the test if we had previously called bail.
+# Test should call this before finishing, but tear_down calls it too.
 sub bail_check {
-    my $self = shift;
-    my $P = $self->{__PACKAGE__."tkstuff"} ||= {};
+    my ($self, $descr) = @_;
+    $descr ||= "Did bail";
+    my $P = $get_priv->($self);
 
     my $probs = delete $P->{bail}; # clear for tear_down
-    $self->fail("Did bail: $probs") if defined $probs;
+    $self->fail("$descr: $probs") if defined $probs;
 }
 
 
 # Register new windows and/or return current existing windows
-# No need to remove, they just become invalid
+# No need to remove, they just become invalid (non-Exist()ent)
 sub tk_windows {
     my ($self, @new_wins) = @_;
-    my $P = $self->{__PACKAGE__."tkstuff"} ||= {};
+    my $P = $get_priv->($self);
     push @{ $P->{windows} }, @new_wins;
-    return @{ $P->{windows} };
+    return grep { $_->Exists } @{ $P->{windows} };
 }
 
 
@@ -89,10 +111,13 @@ sub tk_pokestring {
 # @keysym of the form qw(m a g i c Return)
 sub tk_pokekeysym {
     my ($self, $win, @keysym) = @_;
+#    my $delay = $self->slowdown('key');
     foreach my $key (@keysym) {
 	foreach my $type (qw(<KeyPress> <KeyRelease>)) {
 	    $win->eventGenerate($type, -keysym => $key, -when => 'tail');
 	}
+#	$self->delay($win, $delay);
+#	$win->after($delay) if $delay;
     }
 }
 
